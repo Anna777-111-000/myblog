cbf5992075e6e3c34196190b97274b2e2856ff97
Anna777-111-000 <anneremneva@yandex.ru>
2025-02-14 21:21:16 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)  # Биография может быть необязательной
+    # Другие поля автора
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)  # Для красивых URL
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT) # PROTECT, чтобы нельзя было удалить категорию, если есть посты
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    publication_date = models.DateTimeField(default=timezone.now)
+    tags = models.ManyToManyField(Tag, blank=True) # У поста может не быть тегов
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') # related_name для доступа к комментариям поста
+    author_name = models.CharField(max_length=100)
+    author_email = models.EmailField(blank=True, null=True)
+    content = models.TextField()
+    publication_date = models.DateTimeField(default=timezone.now)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Comment by {self.author_name} on {self.post.title}"
+
